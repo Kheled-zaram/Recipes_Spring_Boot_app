f8b64eedb77f788cfee1e93354bc55b537d71c4d
Agnieszka Klempis <a.klempis@student.uw.edu.pl>
2024-12-31 13:55:59 +0100

    .gitignore is now working

--- build/resources/test/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO RECIPES (id, description, is_sweet, title, url) VALUES(999, null, true, 'Makownik', 'https://ilovebake.pl/przepis/makowiec-na-kruchym-spodzie');
-INSERT INTO RECIPES (id, description, is_sweet, title, url) VALUES(1000, null, false, 'Paszteciki ze szpinakiem', 'https://kuchnia-domowa-ani.blogspot.com/2018/12/paszteciki-ze-szpinakiem-i-serem.html');
-INSERT INTO RECIPES (id, description, is_sweet, title, url) VALUES(1001, null, true, 'Lukier cytrynowy', null);
-
--- RAWTOHEX('Test')
